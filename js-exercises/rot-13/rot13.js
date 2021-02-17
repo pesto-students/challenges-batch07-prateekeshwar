61396181a56c5a48fa5190a988b1b299e2644bf7
prateekeshwar <prateekeshwar@paymeindia.in>
2021-02-17 16:46:51 +0530

    modified rot-13 exercise with inbuilt fromcharcode

--- js-exercises/rot-13/rot13.js
+++ js-exercises/rot-13/rot13.js
@@ -1,18 +1,7 @@
 function rot13(...args) {
   try {
     if (args) {
-      const Alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      let NewDecodeStr = '';
-      for (const CurrentChar of args[0]) {
-        if (CurrentChar.match(/[A-Z]/i)) {
-          const CurentIndex = Alphabets.indexOf(CurrentChar) + 1;
-          NewDecodeStr += CurentIndex + 13 < 27 ? Alphabets.charAt(CurentIndex + 13 - 1)
-            : Alphabets.charAt(CurentIndex + 13 - 26 - 1);
-        } else {
-          NewDecodeStr += CurrentChar;
-        }
-      }
-      return NewDecodeStr;
+      return args[0].replace(/[A-Z]/g, (char) => String.fromCharCode(((char.charCodeAt(0) - 65 + 13) % 26) + 65));
     }
     throw new Error('Null argument');
   } catch (error) {
