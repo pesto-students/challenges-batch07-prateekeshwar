c3c7bbfbfffebe30d429e7547433ba362f1956c5
prateekeshwar <prateekeshwar@paymeindia.in>
2021-02-17 11:34:21 +0530

    used 'of' for looping in character and used terenary operation for conditions instead of if & else

--- js-exercises/rot-13/rot13.js
+++ js-exercises/rot-13/rot13.js
@@ -1,18 +1,13 @@
 function rot13(...args) {
   try {
     if (args) {
-      const RotStr = args[0].toUpperCase();
       const Alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
       let NewDecodeStr = '';
-      for (let i = 0; i < RotStr.length; i += 1) {
-        const CurrentChar = RotStr.charAt(i);
+      for (const CurrentChar of args[0]) {
         if (CurrentChar.match(/[A-Z]/i)) {
           const CurentIndex = Alphabets.indexOf(CurrentChar) + 1;
-          if (CurentIndex + 13 < 27) {
-            NewDecodeStr += Alphabets.charAt(CurentIndex + 13 - 1);
-          } else {
-            NewDecodeStr += Alphabets.charAt(CurentIndex + 13 - 26 - 1);
-          }
+          NewDecodeStr += CurentIndex + 13 < 27 ? Alphabets.charAt(CurentIndex + 13 - 1)
+            : Alphabets.charAt(CurentIndex + 13 - 26 - 1);
         } else {
           NewDecodeStr += CurrentChar;
         }
