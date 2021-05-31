18a7b390cfe7c3cc37eae6d60225c2a92d8d7681
prateekeshwar <prateekeshwar@paymeindia.in>
2021-05-31 13:04:59 +0530

    refactored code

--- js-exercises/balanced-braces/balancedBraces.js
+++ js-exercises/balanced-braces/balancedBraces.js
@@ -2,21 +2,49 @@ function balancedBraces(...args) {
   try {
     const argString = args[0];
     if (args && typeof (args[0] === 'string')) {
-      const bracketArray = [];
-      for (let index = 0; index < argString.length; index += 1) {
-        const char = argString.charAt(index);
-        if (char === '{' || char === '}' || char === '[' || char === ']' || char === '(' || char === ')') {
-          bracketArray.push(char);
+      const bracesMap = {
+        '(': ')',
+        '{': '}',
+        '[': ']',
+      };
+      const openingBraces = Object.keys(bracesMap);
+      const closingBraces = Object.values(bracesMap);
+      const isOpeningBrace = (char) => openingBraces.includes(char);
+      const isClosingBrace = (char) => closingBraces.includes(char);
+      const stack = [];
+      for (const char of argString) {
+        if (isOpeningBrace(char) || isClosingBrace(char)) {
+          if (isOpeningBrace(char)) {
+            stack.push(char);
+          } else {
+            const topItem = stack.pop();
+            if (bracesMap[topItem] !== char) {
+              return false;
+            }
+          }
         }
       }
-      let brackString = bracketArray.join('');
-      while (brackString.length !== 0 && (brackString.includes('[]')
-      || brackString.includes('()') || brackString.includes('{}'))) {
-        brackString = brackString.replace('[]', '');
-        brackString = brackString.replace('()', '');
-        brackString = brackString.replace('{}', '');
+      if (stack.length) {
+        return false;
       }
-      return brackString.length < 1;
+      return true;
+
+      // const bracketArray = [];
+      // for (let index = 0; index < argString.length; index += 1) {
+      //   const char = argString.charAt(index);
+      //   if (char === '{' || char === '}' || char === '[' ||
+      // char === ']' || char === '(' || char === ')'){
+      //     bracketArray.push(char);
+      //   }
+      // }
+      // let brackString = bracketArray.join('');
+      // while (brackString.length !== 0 && (brackString.includes('[]')
+      // || brackString.includes('()') || brackString.includes('{}'))) {
+      //   brackString = brackString.replace('[]', '');
+      //   brackString = brackString.replace('()', '');
+      //   brackString = brackString.replace('{}', '');
+      // }
+      // return brackString.length < 1;
     }
     throw new Error('Invalid or empty argument');
   } catch (error) {
@@ -24,6 +52,4 @@ function balancedBraces(...args) {
   }
 }
 
-export {
-  balancedBraces,
-};
+export { balancedBraces };
--- js-exercises/curry/curry.js
+++ js-exercises/curry/curry.js
@@ -9,6 +9,17 @@ function curry(func) {
   };
 }
 
+// function curry(func) {
+//   return function curried(...initialargs) {
+//     if (initialargs.length >= func.length) {
+//       return func.apply(this, initialargs);
+//     }
+//     return function recursionCurried(...nextargs) {
+//       return curried.apply(this, [...initialargs, ...nextargs]);
+//     };
+//   };
+// }
+
 export {
   curry,
 };
