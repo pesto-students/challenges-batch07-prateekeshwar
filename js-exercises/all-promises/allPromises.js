d2bf99215c7c796d5bdd6af512f60ed8c62ae8d2
prateekeshwar <prateekeshwar@paymeindia.in>
2021-06-05 18:42:57 +0530

    refactored code

--- js-exercises/all-promises/allPromises.js
+++ js-exercises/all-promises/allPromises.js
@@ -1,40 +1,63 @@
-const resolveIndividualPromise = (promise) => {
-  const indPromise = new Promise((resolve, reject) => {
-    if (typeof (promise) === 'string' || typeof (promise) === 'number' || promise === null || promise === undefined) {
-      resolve(promise);
-    } else {
-      promise.then((response) => {
-        resolve(response);
-      }).catch((error) => {
-        reject(error);
-      });
-    }
-  });
-  return indPromise;
-};
-
+/**
+ * Resolve all promise in arra sequentially and return promise of array if
+ * all resolved else return reject promise of first rejected promise.
+ * @param {Array} inputPromiseArr input array of promises that need to be resolved
+ * @param {Array} outputArr default []
+ * @returns {Promise}
+ */
 const recursiveResolvePromise = (inputPromiseArr, outputArr) => {
   const resolvedArr = new Promise((resolve, reject) => {
-    const recursivePromise = (shiftInputPromiseArr, appendOutputArr) => {
-      resolveIndividualPromise(shiftInputPromiseArr[0]).then(indvidualResponse => {
-        appendOutputArr.push(indvidualResponse);
-        shiftInputPromiseArr.shift();
-        if (shiftInputPromiseArr.length > 0) {
-          recursivePromise(shiftInputPromiseArr, appendOutputArr);
-        } else {
-          resolve(appendOutputArr);
-        }
-      }).catch((error) => {
-        reject(error);
-      });
+    const recursivePromise = (
+      shiftInputPromiseArr,
+      appendOutputArr,
+      value = null,
+    ) => {
+      shiftInputPromiseArr[0](value)
+        .then((indvidualResponse) => {
+          appendOutputArr.push(indvidualResponse);
+          shiftInputPromiseArr.shift();
+          if (shiftInputPromiseArr.length > 0) {
+            recursivePromise(
+              shiftInputPromiseArr,
+              appendOutputArr,
+              indvidualResponse,
+            );
+          } else {
+            resolve(appendOutputArr);
+          }
+        })
+        .catch((error) => {
+          reject(error);
+        });
     };
     recursivePromise(inputPromiseArr, outputArr);
   });
   return resolvedArr;
 };
 
-const allPromises = args => {
-  const outputArr = recursiveResolvePromise(args, []);
+/**
+ * Resolve all values in input array or reject the first
+ * exception catched.
+ * @param {Array} args array of promises
+ * @returns {Promise} promise containing resolve or reject
+ */
+const allPromises = (args) => {
+  if (!args) {
+    return Promise.resolve('');
+  }
+  if (!Array.isArray(args)) {
+    throw new Error('Argument is not array');
+  }
+  if (!args.length) {
+    return Promise.resolve([]);
+  }
+  const onlyPromiseFns = args.map((element) => {
+    if (typeof element === 'function') {
+      return element;
+    }
+    return () => Promise.resolve(element);
+  });
+  const outputArr = recursiveResolvePromise(onlyPromiseFns, []);
   return outputArr;
 };
 
--- js-exercises/debounce/debounce.test.js
+++ js-exercises/debounce/debounce.test.js
@@ -1,3 +1,28 @@
-import { debounce } from "./debounce";
-
-describe("debounce", () => {});
+import { debounce } from './debounce';
+jest.useFakeTimers();
+describe('debounce', () => {
+  let func;
+  let debouncedFn;
+  beforeEach(() => {
+    func = jest.fn();
+    // func = () => { console.log("test")}
+    debouncedFn = debounce(func, 2000);
+  });
+  test('executes just once', () => {
+    for (let i = 0; i < 100; i++) {
+      debouncedFn();
+    }
+    jest.runAllTimers();
+    expect(func).toBeCalledTimes(1);
+  });
+  test('not executed immediately', () => {
+    debouncedFn();
+    expect(func).toBeCalledTimes(0);
+  });
+  test('make one call after a delay', () => {
+    debouncedFn();
+    jest.runAllTimers();
+    debouncedFn();
+    expect(func).toBeCalledTimes(1);
+  });
+});
